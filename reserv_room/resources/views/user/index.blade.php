454d5df83035027f75899af19417b2ca790cd4b9
pazcalll <yazeedarifin@gmail.com>
2020-08-03 16:39:17 +0700

    Started with only login page using username

---
+++ reserv_room/resources/views/user/index.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="time.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="js/main.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+</head>
+<body style="margin-bottom: 300px;">
+  <div class="d-flex justify-content-begin">
+    <div class="content" >
+      <h2 class="content-title"><b>Available Room</b></h2>
+    <div class="container d-flex justify-content-begin">
+      <div class="col-12">
+        <div class="row d-flex justify-content-begin" >
+          <div class="col-4">
+            <a href="#" class="content-item-available">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-available">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-available">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-available">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-available">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-available">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-available">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-available">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-available">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-available">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-available">Room Name</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h2 class="content-title"><b>Available Room</b></h2>
+    <div class="container">
+      <div class="col-12">
+        <div class="row d-flex justify-content-begin">
+          <div class="col-4">
+            <a href="#" class="content-item-unavailable">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-unavailable">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-unavailable">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-unavailable">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-unavailable">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-unavailable">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-unavailable">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-unavailable">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-unavailable">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-unavailable">Room Name</a>
+          </div>
+          <div class="col-4">
+            <a href="#" class="content-item-unavailable">Room Name</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <input type="time" class="form-control" value="13:00" >  -->
+
+  </div>
+  <div id="sidebar">
+    <a class="toggle-btn" onclick="toggleSidebar()" href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+    </a>
+    <p>{{ Auth::user()->name }}</p>
+    <p style="padding-top: 0px;">{{ Auth::user()->id }}</p>
+    <ul>
+      <a href="#" style="color: rgba(255,230,230,0.9)";><li>List of Rooms</li></a>
+      <a href="#" style="color: rgba(255,230,230,0.9)";><li>User Management</li></a>
+      <a href="#" style="color: rgba(255,230,230,0.9)";><li>My Room</li></a>
+      <a href="{{route('logout')}}" style="color: rgba(255,230,230,0.9)"; onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();">
+        <li>{{ __('Logout') }}</li>
+      </a>
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+      </form>
+    </ul>
+  </div>
+
+</body>
+</html>
